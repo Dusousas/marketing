8730a7bd0796ea7861c28916e65bf1cbb1960968
dusousas <dusousadeveloper@gmail.com>
2026-02-03 11:19:02 -0300

    3d 1

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,22 +6,25 @@ import Solutions from "./Solutions";
 export default function Hero() {
   return (
     <>
-      <section className="">
-        <div className="maxW flex items-center gap-10">
-
-          <article className="lg:w-1/2">
-            <h1 className="font-sans uppercase">Marketig & produção Audiovisual</h1>
-            <h2 className="font-title text-5xl mt-2">Criamos histórias que vendem</h2>
-            <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ex vitae non voluptatem saepe, nostrum obcaecati tenetur aut!</p>
+      <section className="py-20">
+        <div className="max flex flex-col items-center gap-10">
+          <article className="text-center">
+            <h1 className="font-sans uppercase">
+              Marketig & produção Audiovisual
+            </h1>
+            <h2 className="font-title text-5xl mt-2">
+              Criamos histórias que vendem
+            </h2>
+            <p className="mt-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ex
+              vitae non voluptatem saepe, nostrum obcaecati tenetur aut!
+            </p>
           </article>
 
-          <article>
-            <img src="/test1.png" alt="" />
+          <article className="w-1/2">
+            <Solutions />
           </article>
-
         </div>
-
-        <Solutions />
       </section>
     </>
   );
